7af7b7bc0ff4cdc1ca3d0e0198db335b84fbbe01
barks1212 <willbarkerwork@gmail.com>
2019-08-15 12:14:43 +0100

    setup redux store and initial state with dev tools

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+import App from './containers/App/App';
+import reducer from './store/reducer';
+
+const store = createStore(
+    reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render( app , document.getElementById('root'));
---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import folderData from '../data/folders.json';
+
+const initialState = {
+    folderData,
+    sort: null,
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+}
+
+export default reducer;
