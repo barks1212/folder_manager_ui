4b5ffaf03ed086cb3e1c5dc1a160e868254cfa54
barks1212 <willbarkerwork@gmail.com>
2019-08-18 15:36:40 +0100

    fix import

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import folderDataType from '../../types/types';
+import { folderDataType } from '../../types/types';
 
 const file = ({ contents }) => (
     <div>this is a file</div>
--- src/components/Folder/Folder.js
+++ src/components/Folder/Folder.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import folderDataType from '../../types/types';
+import { folderDataType } from '../../types/types';
 
 const folder = ({ contents }) => (
     <div>
