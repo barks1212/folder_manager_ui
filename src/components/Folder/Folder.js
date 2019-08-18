9bde9d0528cc898e08e9c22097954712fcd7868e
barks1212 <willbarkerwork@gmail.com>
2019-08-18 17:15:08 +0100

    folder to class compoenent

--- src/components/Folder/Folder.js
+++ src/components/Folder/Folder.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import { folderDataType } from '../../types/types';
 
-const folder = ({ contents }) => (
-    <div>
-        this is a folder
-    </div>
-);
 
-folder.propTypes = {
+class Folder extends Component {
+    state = {
+        open: false
+    };
+
+    openFolderHandler (e) {
+        e.preventDefault();
+        this.setState((prevState) => ({open: !prevState.open}));
+    };
+    
+    render () {
+        return (
+            <div onClick={(e) => this.openFolderHandler(e)}>
+                this is a folder
+            </div>
+        )
+    }
+};
+
+Folder.propTypes = {
     contents: PropTypes.shape({
         ...folderDataType,
         files: PropTypes.arrayOf(folderDataType)
     })
 };
 
-export default folder;
+export default Folder;
