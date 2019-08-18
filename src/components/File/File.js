8f4f404aefc308aa7251afaf6e8f0649ae330506
barks1212 <willbarkerwork@gmail.com>
2019-08-18 15:36:02 +0100

    file and folder component proptypes

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const file = () => (
+import folderDataType from '../../types/types';
+
+const file = ({ contents }) => (
     <div>this is a file</div>
 );
+
+file.propTypes = {
+    contents: PropTypes.shape(folderDataType)
+};
 
 export default file;
--- src/components/Folder/Folder.js
+++ src/components/Folder/Folder.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const folder = () => (
+import folderDataType from '../../types/types';
+
+const folder = ({ contents }) => (
     <div>
         this is a folder
     </div>
-)
+);
+
+folder.propTypes = {
+    contents: PropTypes.shape({
+        ...folderDataType,
+        files: PropTypes.arrayOf(folderDataType)
+    })
+};
 
 export default folder;
