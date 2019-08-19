22fd4850cffa994e62286de9baf2c31b02d48da5
barks1212 <willbarkerwork@gmail.com>
2019-08-19 10:14:48 +0100

    react test library test for folderlist list render

--- src/containers/FolderList/FolderList.test.js
+++ src/containers/FolderList/FolderList.test.js
@@ -5,6 +5,7 @@ import { render, cleanup } from '@testing-library/react'
 
 import FolderList from './FolderList';
 import reducer from '../../store/reducer';
+import data from '../../data/folders.json';
 
 const store = createStore(reducer)
 
@@ -18,4 +19,20 @@ describe('folderList', () => {
         </Provider>)
         expect(asFragment()).toMatchSnapshot();
     })
-})
+
+    it('renders a list of files and folders', () => {
+        const {getAllByTestId} = render(
+        <Provider store={store}>
+            <FolderList />)
+        </Provider>
+        );
+        const files = getAllByTestId('file').map(f => f.textContent);
+        const folders = getAllByTestId('folder').map(f => f.textContent);
+
+        const filesToMatch = data.filter(el => el.type !== 'file').map(f => f.name);
+        const foldersToMatch = data.filter(el => el.type === 'folder').map(f => f.name);
+
+        expect(files).toEqual(filesToMatch);
+        expect(folders).toEqual(foldersToMatch);
+    });
+});
