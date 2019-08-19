b94f1dd3076b58dffee81d83c3f497bdd3513d43
barks1212 <willbarkerwork@gmail.com>
2019-08-19 13:00:48 +0100

    tests passing

--- src/components/Folder/Folder.js
+++ src/components/Folder/Folder.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import { folderDataType } from '../../types/types';
 import classes from './Folder.module.css';
 
+import File from '../File/File';
+
 
 class Folder extends Component {
     state = {
@@ -19,17 +21,24 @@ class Folder extends Component {
         const { contents } = this.props;
         const { open } = this.state
 
+        const subFiles = open ? contents.files.map(file => <File key={file.name} contents={file} />) : null;
+
         const chevron = open ? <i className={`fa fa-chevron-up ${classes.Chev}`}></i> : <i className={`fa fa-chevron-down ${classes.Chev}`}></i>
 
         return (
-            <div data-testid="folderWrapper" className={classes.Folder} onClick={(e) => this.openFolderHandler(e)}>
-                <ul>
-                    <li className={classes.Name}>{contents.name}</li>
-                    <li>{chevron}</li>
-                </ul>
-            </div>
-        )
-    }
+            <Fragment>
+                <div data-testid="folderWrapper" className={classes.Folder} onClick={(e) => this.openFolderHandler(e)}>
+                    <ul>
+                        <li className={classes.Name}>{contents.name}</li>
+                        <li>{chevron}</li>
+                    </ul>
+                </div>
+                <div data-testid="subFiles">
+                    {subFiles}
+                </div>
+            </Fragment>
+        );
+    };
 };
 
 Folder.propTypes = {
