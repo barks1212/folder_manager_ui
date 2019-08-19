93c8f483d688f6a0c21ae15d4216fdd1b35b450e
barks1212 <willbarkerwork@gmail.com>
2019-08-19 20:08:10 +0100

    tests for input functionality

---
+++ src/components/Input/Input.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {render, cleanup, fireEvent} from '@testing-library/react';
+
+import Input from './Input';
+
+describe('input', () => {
+
+    it('should render', () => {
+        const filterFilesMock = jest.fn();
+
+        const {asFragment} = render(<Input filterFiles={filterFilesMock}/>);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('should have "Filename..." as a placeholder', () => {
+        const filterFilesMock = jest.fn();
+
+        const {container} = render(<Input filterFiles={filterFilesMock} />);
+        const input = container.querySelector('input');
+
+        expect(input.value).toBe('Filename...');
+    });
+
+    it('should change value on user input', () => {
+        const filterFilesMock = jest.fn();
+
+        const {container} = render(<Input filterFiles={filterFilesMock} />);
+        const input = container.querySelector('input');
+
+        fireEvent.change(input, {target: {value: 'test'}});
+
+        expect(input.value).toBe('test');
+    });
+
+    it('should call filter function on user input', () => {
+        const filterFilesMock = jest.fn();
+
+        const {container} = render(<Input filterFiles={filterFilesMock} />);
+        const input = container.querySelector('input');
+
+        fireEvent.change(input, {target: {value: 't'}});
+
+        expect(filterFilesMock).toHaveBeenCalled();
+    });
+});
