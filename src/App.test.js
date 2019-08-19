2320d55e15404f4d451535b9589138abfa532965
barks1212 <willbarkerwork@gmail.com>
2019-08-19 14:14:29 +0100

    test for dropdown sort

--- src/App.test.js
+++ src/App.test.js
@@ -19,3 +19,36 @@ describe('App', () => {
         expect(asFragment()).toMatchSnapshot();
     });
 });
+
+describe('sort functionality', () => {
+
+    it('should sort by name when name is selected', () => {
+        const { container, getAllByTestId } = render (
+            <Provider store={store}>
+                <App />
+            </Provider>);
+
+        const dropdown = container.querySelector('select');
+        fireEvent.change(dropdown, {target: {value:'name'}});
+
+        const firstFile = getAllByTestId('file')[0].firstElementChild.firstChild.childNodes[1].textContent;
+        const firstFolder = getAllByTestId('folder')[0].firstElementChild.firstChild.childNodes[0].textContent;
+
+        expect(firstFile).toEqual('Cost Centres');
+        expect(firstFolder).toEqual('Expenses');
+    });
+
+    it('should sort by date when date is selected', () => {
+        const { container, getAllByTestId } = render (
+            <Provider store={store}>
+                <App />
+            </Provider>);
+            
+        const dropdown = container.querySelector('select');
+        fireEvent.change(dropdown, {target: {value:'date'}});
+
+        const firstFile = getAllByTestId('file')[0].firstElementChild.firstChild.childNodes[2].textContent;
+
+        expect(firstFile).toEqual('2016-08-12');
+    });
+});
